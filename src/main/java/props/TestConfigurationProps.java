431f42398b12cdca23eac9e073c4f355e6961834
Stephan Anthony <anthony.stephan@gmail.com>
2021-06-09 11:25:03 +0200

    jdbs with properties

--- src/main/java/props/TestConfigurationProps.java
+++ src/main/java/props/TestConfigurationProps.java
@@ -1,7 +1,13 @@
 package props;
 
+import java.util.ResourceBundle;
+
 public class TestConfigurationProps {
     public static void main(String[] args) {
+        ResourceBundle config = ResourceBundle.getBundle("configuration");
+        String nomAdmin = config.getString("configuration.nom");
+        System.out.println(nomAdmin);
+
 
     }
 }
